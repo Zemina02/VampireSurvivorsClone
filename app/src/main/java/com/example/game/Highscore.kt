59038e47191b49cafcae3a32cc7c108c89af583c
Zemina02 <156537921+Zemina02@users.noreply.github.com>
2026-01-11 17:50:54 +0000

    organização pastas

--- app/src/main/java/com/example/game/Highscore.kt
+++
@@ -1,7 +0,0 @@
-package com.example.game.models
-
-data class Highscore(
-    val userId: String = "",
-    val username: String = "",
-    val score: Int = 0
-)
--- app/src/main/java/com/example/game/User.kt
+++
@@ -1,8 +0,0 @@
-package com.example.game.models
-
-data class User (
-    var docId : String? = null,
-    var name : String? = null,
-    var email : String? = null,
-    var bio : String? = null
-)
